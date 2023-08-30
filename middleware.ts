79e711ff86a239c88a7e3727ec62c8cba44b1c88
Kevin Dev <kevin.developer71@gmail.com>
2023-08-29 21:41:17 -0600

    fix clerk infinite loop

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,8 @@ import { authMiddleware } from "@clerk/nextjs"
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ["/api/uploadthing"],
+  domain: process.env.NEXT_PUBLIC_SITE_URL!,
+  isSatellite: false,
 })
 
 export const config = {
