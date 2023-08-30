222ec9a7b19edfe4cbb15e69b8ef179caffa8397
Kevin Dev <kevin.developer71@gmail.com>
2023-08-29 21:46:31 -0600

    debug

--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,7 @@ import { authMiddleware } from "@clerk/nextjs"
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: ["/api/uploadthing"],
-  domain: process.env.NEXT_PUBLIC_SITE_URL!,
-  isSatellite: false,
+  debug: true,
 })
 
 export const config = {
